1ae466703b6d36989b8238f4c01c848fa0f833ef
GohilMs <65270378+GohilMs@users.noreply.github.com>
2020-05-13 09:47:19 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -3,7 +3,7 @@ package daily90;
 public class Palindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 String str ="malayalam";
 String rev="";
 for (int i = str.length()-1; i >=0; i--) {
